b69b7f2f9995aae3a8d91c3b31222d78bb02a559
baku4 <bahkhun@gmail.com>
2025-07-13 03:40:21 +0900

    feat: locate method can use text rev iter & indices

--- src/locate/mod.rs
+++ src/locate/mod.rs
@@ -1,40 +1,10 @@
 use super::{FmIndex, Position, Block};
 
-/// Locate pattern
+mod plain_text;
+mod symbol_indices;
+
+
 impl<'a, P: Position, B: Block> FmIndex<'a, P, B> {
-    // ================================================
-    // Plain text
-    pub fn count_pattern(&self, pattern: &[u8]) -> P {
-        let pos_range = self.get_pos_range(pattern);
-        pos_range.1 - pos_range.0
-    }
-    pub fn locate_pattern(&self, pattern: &[u8]) -> Vec<P> {
-        let pos_range = self.get_pos_range(pattern);
-        self.get_locations(pos_range)
-    }
-
-    fn get_pos_range(&self, pattern: &[u8]) -> (P, P) {
-        let (mut pos_range, mut idx) = self.count_array_view.get_initial_pos_range_and_idx_of_pattern(
-            pattern,
-            &self.encoding_table,
-        );
-        // LF mapping
-        while pos_range.0 < pos_range.1 && idx > 0 {
-            idx -= 1;
-            let next_sym = pattern[idx];
-            pos_range = self.next_pos_range(pos_range, next_sym);
-        }
-        pos_range
-    }
-    fn next_pos_range(&self, pos_range: (P, P), sym: u8) -> (P, P) {
-        let symidx = self.encoding_table.idx_of(sym);
-        let precount = self.count_array_view.get_precount(symidx as usize);
-        let start_rank = self.bwm_view.get_next_rank(pos_range.0, symidx);
-        let end_rank = self.bwm_view.get_next_rank(pos_range.1, symidx);
-        (precount + start_rank, precount + end_rank)
-    }
-
-    // Get index
     fn get_locations(&self, pos_range: (P, P)) -> Vec<P> {
         let mut locations: Vec<P> = Vec::with_capacity((pos_range.1 - pos_range.0).as_usize());
 
---
+++ src/locate/symbol_indices.rs
@@ -0,0 +1,60 @@
+use super::{FmIndex, Position, Block};
+
+/// Locate pattern using plain text
+impl<'a, P: Position, B: Block> FmIndex<'a, P, B> {
+    pub fn count_indices(&self, indices: &[u8]) -> P {
+        let pos_range = self.get_pos_range_of_indices(indices);
+        pos_range.1 - pos_range.0
+    }
+    pub fn locate_indices(&self, indices: &[u8]) -> Vec<P> {
+        let pos_range = self.get_pos_range_of_indices(indices);
+        self.get_locations(pos_range)
+    }
+    pub fn count_indices_rev_iter<I: Iterator<Item = u8>>(&self, indices_rev_iter: I) -> P {
+        let pos_range = self.get_pos_range_from_indices_rev_iter(indices_rev_iter);
+        pos_range.1 - pos_range.0
+    }
+    pub fn locate_indices_rev_iter<I: Iterator<Item = u8>>(&self, indices_rev_iter: I) -> Vec<P> {
+        let pos_range = self.get_pos_range_from_indices_rev_iter(indices_rev_iter);
+        self.get_locations(pos_range)
+    }
+
+    // Get the position range of the indices
+    fn get_pos_range_of_indices(&self, indices: &[u8]) -> (P, P) {
+        let (mut pos_range, mut idx) = self.count_array_view.get_initial_pos_range_and_idx_of_indices(
+            indices,
+        );
+        // LF mapping
+        while pos_range.0 < pos_range.1 && idx > 0 {
+            idx -= 1;
+            let next_idx = indices[idx];
+            pos_range = self.next_pos_range_of_indices(pos_range, next_idx);
+        }
+        pos_range
+    }
+    fn get_pos_range_from_indices_rev_iter<I: Iterator<Item = u8>>(
+        &self,
+        mut indices_rev_iter: I,
+    ) -> (P, P) {
+        let mut pos_range = self.count_array_view.get_initial_pos_range_and_idx_of_indices_rev_iter(
+            &mut indices_rev_iter,
+        );
+        // LF mapping
+        while pos_range.0 < pos_range.1  {
+            match indices_rev_iter.next() {
+                Some(next_idx) => {
+                    pos_range = self.next_pos_range_of_indices(pos_range, next_idx);
+                },
+                None => break,
+            };
+        }
+        pos_range
+    }
+
+    fn next_pos_range_of_indices(&self, pos_range: (P, P), symidx: u8) -> (P, P) {
+        let precount = self.count_array_view.get_precount(symidx as usize);
+        let start_rank = self.bwm_view.get_next_rank(pos_range.0, symidx);
+        let end_rank = self.bwm_view.get_next_rank(pos_range.1, symidx);
+        (precount + start_rank, precount + end_rank)
+    }
+}
