f70c37bbe096fde61825ecb4269e06655ca66d4a
baku4 <bahkhun@gmail.com>
2025-07-18 16:30:44 +0900

    fix: linter errors in crate benches

--- sview-fmindex/benches/locate_with_raw_index/mod.rs
+++ sview-fmindex/benches/locate_with_raw_index/mod.rs
@@ -3,9 +3,11 @@ use criterion::{
     PlotConfiguration, AxisScale,
 };
 use sview_fmindex::{
-    LtFmIndex, blocks::{
+    FmIndex, blocks::{
         Block2, Block3, Block4,
     }, Position, Block,
+    build_config::{LookupTableConfig, SuffixArrayConfig},
+    FmIndexBuilder,
 };
 use super::random_data::{
     gen_rand_text,
@@ -13,44 +15,34 @@ use super::random_data::{
 };
 use std::time::{Duration, Instant};
 
-type Lfi32_2_32 = LtFmIndex<u32, Block2<u32>>;
-type Lfi32_2_64 = LtFmIndex<u32, Block2<u64>>;
-type Lfi32_2_128 = LtFmIndex<u32, Block2<u128>>;
-type Lfi32_3_32 = LtFmIndex<u32, Block3<u32>>;
-type Lfi32_3_64 = LtFmIndex<u32, Block3<u64>>;
-type Lfi32_3_128 = LtFmIndex<u32, Block3<u128>>;
-type Lfi32_4_32 = LtFmIndex<u32, Block4<u32>>;
-type Lfi32_4_64 = LtFmIndex<u32, Block4<u64>>;
-type Lfi32_4_128 = LtFmIndex<u32, Block4<u128>>;
-type Lfi64_2_32 = LtFmIndex<u64, Block2<u32>>;
-type Lfi64_2_64 = LtFmIndex<u64, Block2<u64>>;
-type Lfi64_2_128 = LtFmIndex<u64, Block2<u128>>;
-type Lfi64_3_32 = LtFmIndex<u64, Block3<u32>>;
-type Lfi64_3_64 = LtFmIndex<u64, Block3<u64>>;
-type Lfi64_3_128 = LtFmIndex<u64, Block3<u128>>;
-type Lfi64_4_32 = LtFmIndex<u64, Block4<u32>>;
-type Lfi64_4_64 = LtFmIndex<u64, Block4<u64>>;
-type Lfi64_4_128 = LtFmIndex<u64, Block4<u128>>;
+fn get_decoding_table<P: Position, B: Block>(fi: &FmIndex<P, B>) -> Vec<u8> {
+    let encoding_table = fi.get_encoding_table();
+    let mut decoding_table = vec![0; 256];
+    for (i, c) in encoding_table.iter().enumerate() {
+        decoding_table[*c as usize] = i as u8;
+    }
+    decoding_table
+}
 
 #[inline]
-fn locate_multiple_patterns<P: Position, B: Block<P>>(
-    lfi: &LtFmIndex<P, B>,
+fn locate_multiple_patterns<P: Position, B: Block>(
+    lfi: &FmIndex<P, B>,
     patterns: &[Vec<u8>]
 ) {
     patterns.iter().for_each(|pattern| {
-        _ = lfi.locate(pattern);
+        _ = lfi.locate_pattern(pattern);
     });
 }
 
 #[inline]
-fn locate_multiple_patterns_from_raw_index<P: Position, B: Block<P>>(
-    lfi: &LtFmIndex<P, B>,
+fn locate_multiple_patterns_from_raw_index<P: Position, B: Block>(
+    fi: &FmIndex<P, B>,
     patterns: &[Vec<u8>]
 ) {
     patterns.iter().for_each(|pattern| {
-        let decoding_table = lfi.decoding_table();
+        let decoding_table = get_decoding_table(fi);
         let raw_index_rev_iter = pattern.iter().map(|&c| decoding_table[c as usize]).rev();
-        _ = lfi.locate_from_raw_index(raw_index_rev_iter);
+        _ = fi.locate_pattern_rev_iter(raw_index_rev_iter);
     });
 }
 
@@ -77,17 +69,24 @@ pub fn compare_locate_vs_locate_from_raw_index(c: &mut Criterion) {
     for ss in ss_list {
         for lk in lk_list {
             println!("# SS: {}, LK: {}", ss, lk);
+
+            
+
             macro_rules! TestCode {
-                ( $lfity: ident, $tagprefix: tt ) => {
+                ( $pos: ty, $blk: ty, $tagprefix: tt) => {
                     {
                         let tag = format!("{}_ss{}_lk{}", $tagprefix, ss, lk);
-                        let lfi = {
-                            let start = Instant::now();
-                            let lfi = $lfity::build(text.clone(), characters_by_index, ss as _, lk).unwrap();
-                            let duration = start.elapsed();
-                            println!(" - {}: built in {:?}s", tag, duration);
-                            lfi
-                        };
+
+                        // Build FM-index
+                        let start = Instant::now();
+                        let builder = FmIndexBuilder::<$pos, $blk>::init(text.len(),  &characters_by_index).unwrap()
+                            .set_suffix_array_config(SuffixArrayConfig::Compressed(ss)).unwrap()
+                            .set_lookup_table_config(LookupTableConfig::KmerSize(lk)).unwrap();
+                        let mut blob = Vec::new();
+                        builder.build(text.clone(), &mut blob).unwrap();
+                        let fi = FmIndex::<$pos, $blk>::load(&blob).unwrap();
+                        let duration = start.elapsed();
+                        println!(" - {}: built in {:?}s", tag, duration);
 
                         for (pattern_len, patterns) in pattern_length.iter().zip(patterns_by_length.iter()) {
                             group.bench_with_input(
@@ -95,7 +94,7 @@ pub fn compare_locate_vs_locate_from_raw_index(c: &mut Criterion) {
                                 &pattern_len,
                                 |b, _i| b.iter(|| {
                                     locate_multiple_patterns(
-                                        black_box(&lfi),
+                                        black_box(&fi),
                                         black_box(patterns),
                                     );
                                 }
@@ -106,7 +105,7 @@ pub fn compare_locate_vs_locate_from_raw_index(c: &mut Criterion) {
                                 &pattern_len,
                                 |b, _i| b.iter(|| {
                                     locate_multiple_patterns_from_raw_index(
-                                        black_box(&lfi),
+                                        black_box(&fi),
                                         black_box(patterns),
                                     );
                                 }
@@ -115,17 +114,11 @@ pub fn compare_locate_vs_locate_from_raw_index(c: &mut Criterion) {
                     }
                 };
             }
-            TestCode!(Lfi32_2_32, "LFI_u32_b2_v32");
-            // TestCode!(Lfi32_2_64, "LFI_u32_b2_v64");
-            // TestCode!(Lfi32_2_128, "LFI_u32_b2_v128");
-            // TestCode!(Lfi32_4_32, "LFI_u32_b4_v32");
-            TestCode!(Lfi32_4_64, "LFI_u32_b4_v64");
-            // TestCode!(Lfi32_4_128, "LFI_u32_b4_v128");
-            TestCode!(Lfi64_2_32, "LFI_u64_b2_v32");
-            // TestCode!(Lfi64_2_64, "LFI_u64_b2_v64");
-            // TestCode!(Lfi64_2_128, "LFI_u64_b2_v128");
-            // TestCode!(Lfi64_4_32, "LFI_u64_b4_v32");
-            TestCode!(Lfi64_4_64, "LFI_u64_b4_v64");
+            TestCode!(u32, Block2<u64>, "FI_u32_b2_v64");
+            TestCode!(u32, Block4<u64>, "FI_u32_b4_v64");
+
+            TestCode!(u64, Block2<u64>, "FI_u64_b2_v64");
+            TestCode!(u64, Block4<u64>, "FI_u64_b4_v64");
         }
     }
 
